d9fbb53663a78e4f806d40bd68910110bc9dd6a4
MARCO BOI <18180017@reginaassumpta.qc.ca>
2023-10-14 10:52:22 -0400

    first commit

--- index.php
+++ index.php
@@ -11,11 +11,9 @@
         <?php if (isset($_GET['error'])) { ?>
             <p class="error"><?php echo $_GET['error']; ?></p>
         <?php } ?>
+        <?php echo $_GET['group']; ?>
         <label>User Name</label>
         <input type="text" name="uname" placeholder="User Name"><br>
-
-        <!-- <label>Password</label>
-        <input type="password" name="password" placeholder="Password"><br> -->
 
         <button type="submit">Go</button>
     </form>
