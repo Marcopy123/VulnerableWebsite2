4c408d948b4c9f0860eab316ce98f76f6220c7ee
Tony <letuanhuynguyen@gmail.com>
2023-10-14 11:34:30 -0400

    bad website style

--- index.php
+++ index.php
@@ -6,15 +6,20 @@
     </head>
 
 <body>
-    <form action="login.php" method="post">
-        <h2>Enter your name!</h2>
-        <?php if (isset($_GET['error'])) { ?>
-            <p class="error"><?php echo $_GET['error']; ?></p>
-        <?php } ?>
-        <?php echo $_GET['group']; ?>
-        <label>User Name</label>
-        <input type="text" name="uname" placeholder="User Name"><br>
+    <img src="./Images/hacker.png" class="bg">
+    <div class="welcome">
+        <h1 class="textWelcome">Welcome fellow mellows</h1>
+        <h2 class="textWelcome">Please log in, your information is safe.</h2>
+        <form class="login" action="login.php" method="post">
+            
+            <?php if (isset($_GET['error'])) { ?>
+                <p class="error"><?php echo $_GET['error']; ?></p>
+            <?php } ?>
+            <?php echo $_GET['group']; ?>
+            <label class="prompt">User Name</label>
+            <input class="input" type="text" name="uname" placeholder="User Name"><br>
 
-        <button type="submit">Go</button>
-    </form>
+            <button type="submit">Go</button>
+        </form>
+    </div>
 </body>
