0d57d5d95b6700ae7cad476dad51a14f9adcb546
Tony <letuanhuynguyen@gmail.com>
2023-10-14 10:36:46 -0400

    Added style to login page

--- index.php
+++ index.php
@@ -6,17 +6,19 @@
     </head>
 
 <body>
-    <form action="login.php" method="post">
-        <h2>Enter your name!</h2>
-        <?php if (isset($_GET['error'])) { ?>
-            <p class="error"><?php echo $_GET['error']; ?></p>
-        <?php } ?>
-        <label>User Name</label>
-        <input type="text" name="uname" placeholder="User Name"><br>
-
-        <!-- <label>Password</label>
-        <input type="password" name="password" placeholder="Password"><br> -->
-
-        <button type="submit">Go</button>
-    </form>
+    <img src="./Images/hacker.png" class="bg">
+    <div class="welcome">
+        <h1 class="textWelcome">Welcome fellow mellows</h1>
+        <h2 class="textWelcome">Please log in, your information is safe.</h2>
+        <form action="login.php" method="post" class="login">
+            <?php if (isset($_GET['error'])) { ?>
+                <p class="error"><?php echo $_GET['error']; ?></p>
+            <?php } ?>
+            <label for="username" class="prompt">Username:</label>
+            <input type="text" name="uname" placeholder="User Name" class="input"><br>
+            <label for="password" class="prompt">Password:</label>
+            <input class="input" type="password" name="password" placeholder="Password"><br>
+            <button type="submit">Go</button>
+        </form>
+    </div>
 </body>
